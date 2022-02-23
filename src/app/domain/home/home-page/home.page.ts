cdc54684fa78766ec5186100b15b18d1d9a30f80
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-02-23 11:54:54 -0300

    ✨ feat(home-page): search card

--- src/app/domain/home/home-page/home.page.ts
+++ src/app/domain/home/home-page/home.page.ts
@@ -1,11 +1,36 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-
+import { Immobile } from '..';
+import MOCK_DATA from '../../../../database/MOCK_DATA.json';
 @Component({
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage implements OnInit {
+  MOCK_DATA = MOCK_DATA;
+  LOCATIONS = [
+    ...new Set(MOCK_DATA.map((immobile: Immobile) => immobile.address.city)),
+  ];
+  DISTRICTS = [
+    ...new Set(
+      MOCK_DATA.map((immobile: Immobile) => immobile.address.district)
+    ),
+  ];
+  BEDROOMS = [
+    ...new Set(MOCK_DATA.map((immobile: Immobile) => immobile.bedrooms)),
+  ];
+  VALUES = [
+    ...new Set(MOCK_DATA.map((immobile: Immobile) => immobile.value)),
+  ].sort((a, b) => a - b);
+
+  propertiesTypes = [
+    'Apartment',
+    'Home',
+    'Triplex house',
+    'Duplex house',
+    'Family Farm',
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/domain/home/home.domain.module.ts
+++ src/app/domain/home/home.domain.module.ts
@@ -1,10 +1,29 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { HomePage } from './home-page/home.page';
 import { HomeDomainRoutingModule } from './home.domain-routing.module';
 
 @NgModule({
   declarations: [HomePage],
-  imports: [CommonModule, HomeDomainRoutingModule],
+  imports: [
+    CommonModule,
+    MatButtonToggleModule,
+    HomeDomainRoutingModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatIconModule,
+  ],
 })
 export class HomeDomainModule {}
